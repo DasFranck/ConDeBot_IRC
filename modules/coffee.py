e34d6ac83fc5b8addec1fe51190b027ee6f435e0
DasFranck <dasfranck@gmx.com>
2016-11-28 18:10:40 +0100

    Just adding one quote for the coffee

--- modules/coffee.py
+++ modules/coffee.py
@@ -5,7 +5,8 @@
 NAME = "ConDeBot"
 QUOTES = ["Without sugar.", "With one sugar.", "With too many sugar.",
           "With a pinch of sugar.", "With three spoon of salt because I hate you. Really.",
-          "With a bit of milk.", "Wait did I put a sugar? I'm not sure...", ""]
+          "With a bit of milk.", "Wait did I put a sugar? I'm not sure...",
+          "Decaffeinated, cause it's way too strong for a little weak buddy like you.", ""]
 
 
 try:
